0ea07f78440696484cdcb13d98fdc45f21c1a76c
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-03-01 11:57:57 +0000

    added style to main navigation

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
+import classes from "./MainNavigation.module.css";
+
 function MainNavigation() {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
-        <ul>
+        <ul className={classes.list}>
           <li>
             <Link to="/">Home</Link>
           </li>
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
+import classes from "./Root.module.css";
+
 function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <Outlet />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
     </>
   );
 }
