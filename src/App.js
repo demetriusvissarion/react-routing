c6482c45af3a1dcf2b223fe3ae71d6d8c740ea01
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-03-01 12:17:16 +0000

    added error page and merged styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   RouterProvider,
   // Route,
 } from "react-router-dom";
+import ErrorPage from "./pages/Error";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
