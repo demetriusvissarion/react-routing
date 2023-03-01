093a2ea8677471cf4f1466d68aec768d89b007fd
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-03-01 12:47:43 +0000

    dynamic path segments

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import ErrorPage from "./pages/Error";
 
 import HomePage from "./pages/Home";
+import ProductDetailPage from "./pages/ProductDetail";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 
@@ -25,6 +26,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
